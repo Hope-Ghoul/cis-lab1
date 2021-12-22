44cee7dda340b3064db71e60be12c1d713fe4de3
Hope-Ghoul <70754818+Hope-Ghoul@users.noreply.github.com>
2021-12-22 15:59:19 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,31 @@
+var http = require("http"),
+  url = require("url");
+
+var days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+var now = new Date();
+var dayofweek = now.getDay();
+
+//create a server object:
+http
+  .createServer(function (req, res) {
+    var query = url.parse(req.url, true).query;
+    res.write("Hello ");
+    if ("name" in query) {
+      res.write(query["name"]);
+    } else {
+      res.write("noname");
+    }
+    res.write(", today is ");
+    res.write(days[dayofweek]);
+    res.end(); //end the response
+  })
+  .listen(8080); //the server object listens on port 8080
